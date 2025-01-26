1f8b7c0eb84273b59251530dc1039a0c001830e7
stswoon <xx@xx.xx>
2025-01-26 20:33:26 +0400

    refactoring

--- src/engine/processIndex.mjs
+++ src/engine/processIndex.mjs
@@ -3,32 +3,54 @@ import fs from "node:fs";
 import {BLOG_RESULT} from "./globals.mjs";
 import {srcDirName, nodeModulesDirName, buildDirName} from "./constants.mjs";
 import {md} from "./init-md.mjs";
-import {resolveSpecificMacros} from "./resolveMacros.mjs";
+import {resolveSeveralMacros, resolveSpecificMacros} from "./resolveMacros.mjs";
 
 export function generateIndex() {
     console.info("generateIndex: start");
 
     copyStaticAssets();
 
-    const tocHtml = generateTocHtml();
-    const last5Html = generateLast5Html();
+    // const tocHtml = generateTocHtml();
+    // const last5Html = generateLast5Html();
+    const allShortHtml = generateAllShortHtml();
 
     let data = fs.readFileSync(
         path.join(srcDirName, "templates/index.html"),
         "utf8"
     );
-    data = resolveSpecificMacros(data, "last_5", last5Html);
-    data = resolveSpecificMacros(data, "menu", tocHtml);
+    data = resolveSpecificMacros(data, "allShortHtml", allShortHtml);
+    // data = resolveSpecificMacros(data, "last_5", last5Html);
+    // data = resolveSpecificMacros(data, "menu", tocHtml);
     data = resolveSpecificMacros(data, "title", BLOG_RESULT.meta.title);
     data = resolveSpecificMacros(data, "description", BLOG_RESULT.meta.description);
-    data = resolveSpecificMacros(data, "last_5", last5Html);
     data = resolveSpecificMacros(data, "ad_head", BLOG_RESULT.adHeadHtml);
-    data = resolveSpecificMacros(data, "ad_block", BLOG_RESULT.adBlockHtml);
+    // data = resolveSpecificMacros(data, "ad_block", BLOG_RESULT.adBlockHtml);
     data = resolveSpecificMacros(data, "footer", BLOG_RESULT.footerHtml);
-    data = resolveSpecificMacros(data, "last_5", last5Html);
     fs.writeFileSync(path.join(buildDirName, "index.html"), data, "utf-8");
 
     console.info("generateIndex: finish");
+}
+
+function generateAllShortHtml() {
+    const paperItem = fs.readFileSync(path.join(srcDirName, "templates/index_paper-item.html"), "utf8");
+
+    let result = "";
+    for (let i = 0; i < BLOG_RESULT.pages.length; ++i) {
+        const pageData = BLOG_RESULT.pages[i]
+        const paperMacrosData = {
+            "page_short_html": pageData.shortHtml,
+            "page_link": pageData.link
+        }
+        result += resolveSeveralMacros(paperItem, paperMacrosData) + "\n";
+
+        if (i === 5 || i === BLOG_RESULT.pages.length - 1) {
+            let adHtml = '<div class="paper-item ad">{@blogEngine:ad_block}</div>';
+            adHtml = resolveSpecificMacros(adHtml, "ad_block", BLOG_RESULT.adBlockHtml);
+            result += adHtml + "\n";
+        }
+    }
+    // console.log("result=", result);
+    return result;
 }
 
 function generateLast5Html() {
@@ -36,25 +58,29 @@ function generateLast5Html() {
 
     let result = "";
     for (let i = 0; i < Math.min(BLOG_RESULT.pages.length, 5); ++i) {
-        const shortHtml = BLOG_RESULT.pages[i].shortHtml;
-        result += resolveSpecificMacros(paperItem, "page_short_html", shortHtml) + "\n";
+        const pageData = BLOG_RESULT.pages[i]
+        const paperMacrosData = {
+            "page_short_html": pageData.shortHtml,
+            "page_link": pageData.link
+        }
+        result += resolveSeveralMacros(paperItem, paperMacrosData) + "\n";
     }
     // console.log("result=", result);
     return result;
 }
 
-//TODO: make md generation, then html replace because this function also need in page
-function generateTocHtml() {
-    //console.log("blogResultModel=", blogResultModel);
-    let tocMd = "";
-    for (const page of BLOG_RESULT.pages) {
-        const title = new Date(page.date).getFullYear() + ": " + page.title;
-        tocMd += `* [${title}](${page.link})` + "\n";
-    }
-    const tocHtml = md.render(tocMd);
-    //console.log("tocHtml=", tocHtml);
-    return tocHtml;
-}
+// //TODO: make md generation, then html replace because this function also need in page
+// function generateTocHtml() {
+//     //console.log("blogResultModel=", blogResultModel);
+//     let tocMd = "";
+//     for (const page of BLOG_RESULT.pages) {
+//         const title = new Date(page.date).getFullYear() + ": " + page.title;
+//         tocMd += `* [${title}](${page.link})` + "\n";
+//     }
+//     const tocHtml = md.render(tocMd);
+//     //console.log("tocHtml=", tocHtml);
+//     return tocHtml;
+// }
 
 function copyStaticAssets() {
     fs.copyFileSync(
--- src/engine/processPages.mjs
+++ src/engine/processPages.mjs
@@ -1,6 +1,7 @@
 import fs from "node:fs";
 import path from "path";
 import {globSync} from "glob";
+import {styleText} from 'node:util';
 import {md} from "./init-md.mjs";
 import {srcDirName, buildDirName} from "./constants.mjs";
 import {BLOG_RESULT} from "./globals.mjs";
@@ -42,8 +43,9 @@ function pageGeneration(srcPageDirName) {
 
     copyStaticAssets(srcPageDirName, buildPageDirName);
 
-    const pageHtml = generatePageHtml(path.join(srcPageDirName, meta.fileName));
-    const shortHtml = generatePageHtml(path.join(srcPageDirName, meta.fileName), true);
+    const srcPageFileName = path.join(srcPageDirName, meta.fileName)
+    const pageHtml = generatePageHtml(srcPageFileName);
+    const shortHtml = generatePageShortHtml(srcPageFileName);
     BLOG_RESULT.pages.push({
         meta,
         link,
@@ -52,25 +54,38 @@ function pageGeneration(srcPageDirName) {
     });
 
     let data = fs.readFileSync(path.join(srcDirName, "templates/paper.html"), "utf8");
+    data = resolveSpecificMacros(data, "title", BLOG_RESULT.meta.title);
     data = resolveSpecificMacros(data, "page_title", meta.title);
-    data = resolveSpecificMacros(data,"page_description", meta.description);
-    data = resolveSpecificMacros(data,"page_html", pageHtml);
-    data = resolveSpecificMacros(data,"ad_head", BLOG_RESULT.adHeadHtml);
-    data = resolveSpecificMacros(data,"ad_block", BLOG_RESULT.adBlockHtml);
-    data = resolveSpecificMacros(data,"footer", BLOG_RESULT.footerHtml);
+    data = resolveSpecificMacros(data, "page_description", meta.description);
+    data = resolveSpecificMacros(data, "page_html", pageHtml);
+    data = resolveSpecificMacros(data, "ad_head", BLOG_RESULT.adHeadHtml);
+    data = resolveSpecificMacros(data, "ad_block", BLOG_RESULT.adBlockHtml);
+    data = resolveSpecificMacros(data, "footer", BLOG_RESULT.footerHtml);
     fs.writeFileSync(buildPageFileName, data, "utf-8");
 }
 
-function generatePageHtml(srcPageFileName, short) {
+function generatePageShortHtml(srcPageFileName) {
     let pageData = fs.readFileSync(srcPageFileName, "utf8");
-    let pageHtml;
-    if (short) {
-        //TODO:remove images and etc, use only paragraph
-        pageData = pageData.substring(0, 100) + "...";
-        pageHtml = md.render(pageData);
+    let pageShortHtml;
+    pageData = pageData.replaceAll("\r\n", "\n");
+    let secondParagraphEnd = pageData.indexOf("\n\n");
+    secondParagraphEnd = pageData.indexOf("\n\n", secondParagraphEnd + 1);
+    if (secondParagraphEnd === -1) {
+        console.warn(styleText(
+            'yellowBright',
+            `Cannot find first paragraph in parer so it cause display all paper in index page. Warning for paper: ${srcPageFileName}`
+        ));
     } else {
-        pageHtml = md.render(pageData);
+        pageData = pageData.substring(0, secondParagraphEnd);
     }
+    pageShortHtml = md.render(pageData);
+    // console.log("pageShortHtml:", pageHtml);
+    return pageShortHtml;
+}
+
+function generatePageHtml(srcPageFileName) {
+    let pageData = fs.readFileSync(srcPageFileName, "utf8");
+    let pageHtml = md.render(pageData);
     // console.log("pageHtml:", pageHtml);
     return pageHtml;
 }
